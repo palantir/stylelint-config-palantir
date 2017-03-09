palantir/stylelint-config-palantir

commit 9cbe28af15a44109e44f05d623740ccded2e6b6a

Upgrade stylelint to v7.8.0 and resolve deprecations (#10)

sass.js
@@ -11,7 +11,7 @@ module.exports = {
   "rules": {
     "at-rule-empty-line-before": ["always", {
       except: ["first-nested"],
-      ignore: ["after-comment", "blockless-group"],
+      ignore: ["after-comment", "blockless-after-blockless"],
       // allow @else to come on same line as closing @if brace
       ignoreAtRules: ["else"],
     }],

stylelint.config.js
@@ -9,6 +9,10 @@ module.exports = {
   // export namingPattern so it can be used in your own rules
   namingPattern,
 
+  plugins: [
+    "stylelint-order",
+  ],
+
   rules: {
     "at-rule-no-unknown": true,
     "at-rule-no-vendor-prefix": true,
@@ -16,14 +20,13 @@ module.exports = {
     "color-hex-length": "long",
     "color-named": "never",
     "declaration-block-no-duplicate-properties": true,
-    // property order is defined in a separate file for legibility
-    "declaration-block-properties-order": [require("./property-order.js"), {
-      unspecified: "bottomAlphabetical",
-    }],
     "declaration-block-semicolon-newline-after": "always",
     "declaration-block-semicolon-newline-before": "never-multi-line",
     "declaration-colon-newline-after": null,
     "declaration-colon-space-after": "always",
+    "declaration-empty-line-before": ["never", {
+      ignore: ["after-declaration"],
+    }],
     "declaration-no-important": true,
     "font-family-name-quotes": "always-unless-keyword",
     "font-weight-notation": "numeric",
@@ -43,8 +46,7 @@ module.exports = {
     "number-no-trailing-zeros": true,
     "property-no-unknown": true,
     "property-no-vendor-prefix": true,
-    "root-no-standard-properties": true,
-    "rule-nested-empty-line-before": ["always-multi-line", {
+    "rule-empty-line-before": ["always-multi-line", {
       except: ["first-nested"],
       ignore: ["after-comment"],
     }],
@@ -57,13 +59,24 @@ module.exports = {
     "selector-no-id": true,
     "selector-no-universal": true,
     "selector-no-vendor-prefix": true,
-    "selector-root-no-composition": true,
     "string-quotes": "double",
-    "stylelint-disable-reason": "always-before",
-    "time-no-imperceptible": true,
+    "time-min-milliseconds": 100,
     "unit-blacklist": ["pt"],
     "value-keyword-case": "lower",
     "value-list-comma-newline-before": "never-multi-line",
     "value-no-vendor-prefix": true,
+    "order/declaration-block-order": [[
+      "custom-properties",
+      "dollar-variables",
+      "declarations",
+      "rules",
+      "at-rules",
+    ], {
+      unspecified: "bottom"
+    }],
+    // property order is defined in a separate file for legibility
+    "order/declaration-block-properties-specified-order": [require("./property-order.js"), {
+      unspecified: "bottomAlphabetical",
+    }],
   },
 };

test/example.css
@@ -2,6 +2,10 @@ html {
     box-sizing: border-box;
 }
 
+a {
+    animation: 100ms;
+}
+
 .overlay {
     position: absolute;
     top: 0;

commit b82c1e38106aef8a9d32e7aca8dc1f49dced2b2b

Fix some config regressions introduced in #10

sass.js
@@ -10,10 +10,10 @@ module.exports = {
   ],
   "rules": {
     "at-rule-empty-line-before": ["always", {
-      except: ["first-nested"],
-      ignore: ["after-comment", "blockless-after-blockless"],
+      except: ["blockless-after-blockless", "first-nested"],
+      ignore: ["after-comment"],
       // allow @else to come on same line as closing @if brace
-      ignoreAtRules: ["else"],
+      ignoreAtRules: ["import", "else"],
     }],
     "at-rule-no-unknown": [true, {
       ignoreAtRules: [

stylelint.config.js
@@ -65,18 +65,25 @@ module.exports = {
     "value-keyword-case": "lower",
     "value-list-comma-newline-before": "never-multi-line",
     "value-no-vendor-prefix": true,
-    "order/declaration-block-order": [[
-      "custom-properties",
-      "dollar-variables",
-      "declarations",
-      "rules",
-      "at-rules",
-    ], {
-      unspecified: "bottom"
-    }],
+    "order/declaration-block-order": [
+      [
+        "custom-properties",
+        "at-variables",
+        "dollar-variables",
+        "less-mixins",
+        {
+          "type": "at-rule",
+          "name": "include"
+        },
+        "declarations",
+        "rules",
+      ],
+      { unspecified: "ignore" }
+    ]
     // property order is defined in a separate file for legibility
-    "order/declaration-block-properties-specified-order": [require("./property-order.js"), {
-      unspecified: "bottomAlphabetical",
-    }],
+    "order/declaration-block-properties-specified-order": [
+      require("./property-order.js"),
+      { unspecified: "bottomAlphabetical" }
+    ],
   },
 };