9e770ee30c50e11b2f3e5652514a0a7ce8853d9b
Gilad Gray <giladgray@gmail.com>
2018-03-15 11:07:46 -0700

    add background-clip

--- property-order.js
+++ property-order.js
@@ -91,10 +91,11 @@ module.exports = [
 
   // Content dimensions and background and scrollbars
   "background",
+  "background-clip",
   "background-color",
   "background-image",
-  "background-repeat",
   "background-position",
+  "background-repeat",
   "background-size",
   "cursor",
 
